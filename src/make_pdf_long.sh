2b0724561d8bba61f2999aff6058aeeba06a43c5
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-12-13 17:57:14 -0600

    pdf long testing

---
+++ src/make_pdf_long.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#
+# Produce PDF reports
+
+# Find longitudinal subject dirs
+subj_dirs=$(ls -d ${SUBJECTS_DIR}/*.long.*)
+subj_dirs=${subj_dirs//$'\n'/ }
+
+
+# Source and working dirs
+export src_dir=$(realpath $(dirname $(which ${0})))
+export tmp_dir="${out_dir}"/imgtmp && mkdir -p "${tmp_dir}"
+export XDG_RUNTIME_DIR="${tmp_dir}"/runtime-root
+
+
+# Make screenshots and PDFs
+export the_date=$(date)
+let c=0
+for subj_dir in ${subj_dirs}; do
+
+    (( c ++ ))
+    cstr=$(printf "%03d" ${c})
+
+    export subj_dir
+    export mri_dir="${subj_dir}"/mri
+    export surf_dir="${subj_dir}"/surf
+
+    page1.sh
+    page2.sh
+    page3.sh
+    page4.sh
+    convert \
+        "${tmp_dir}"/page1.png \
+        "${tmp_dir}"/page2.png \
+        "${tmp_dir}"/page3.png \
+        "${tmp_dir}"/page4.png \
+        "${tmp_dir}"/Freesurfer-QA-${cstr}.pdf
+
+done
+
+mkdir -p "${out_dir}"/PDF
+convert \
+    "${tmp_dir}"/Freesurfer-QA-*.pdf \
+    "${out_dir}"/PDF/Freesurfer-QA.pdf
+
+
+# Detailed PDF
+#make_slice_screenshots.sh
